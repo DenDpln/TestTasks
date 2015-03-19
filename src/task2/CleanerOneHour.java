2360c3057cdebf31d0611d04c49be2355536bbe9
DenDpl <den.dpl@gmail.com>
2015-03-19 23:42:19 +0300

    thread

--- src/task2/CleanerOneHour.java
+++ src/task2/CleanerOneHour.java
@@ -1,22 +1,26 @@
-//package task2;
-//
-//import java.io.IOException;
-//
-///**
-// * Created by den on 19.03.2015.
-// */
-//public class CleanerOneHour extends Thread{
-//
-//    @Override
-//    public void run() {
-//        while (!Thread.currentThread().isInterrupted()) {
-//            try {
-//                // my code goes here
-//           // } catch (InterruptedException ex) {
-//                Thread.currentThread().interrupt();
-//            }
-//        }
-//    }
+package task2;
+
+import java.io.IOException;
+
+/**
+* Created by den on 19.03.2015.
+*/
+public class CleanerOneHour extends Thread{
+
+    @Override
+    public void run() {
+       // while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.printf("con nachalo ");
+                Thread.sleep(10000);
+                System.out.println("zapusk cleaner");
+
+            } catch (InterruptedException ex) {
+                System.out.printf(" ostanov ");
+                Thread.currentThread().run();
+
+            }
+        }
+    }
 //}
-//
-//}
+
--- src/task2/StoreCash.java
+++ src/task2/StoreCash.java
@@ -6,42 +6,54 @@ import java.util.Set;
  * Created by den on 19.03.2015.
  */
 public class StoreCash {
+   public static CleanerOneHour clHour = null;
    public static boolean  monitor = false;
     public static void messagesCome(){
-
+        clHour = new CleanerOneHour();
+        clHour.start();
         try {
+            //Thread.sleep(500);
+            JobCHMap.putCHMap(System.currentTimeMillis(), "mess2");
             cleanerOnTime();
-            JobCHMap.putCHMap(System.currentTimeMillis(),"mess2");
-            cleanerOnTime();
+
             Thread.sleep(111);
             JobCHMap.putCHMap(System.currentTimeMillis(),"mess3");
             cleanerOnTime();
+
             Thread.sleep(544);
             JobCHMap.putCHMap(System.currentTimeMillis(),"mess4");
             cleanerOnTime();
+
             Thread.sleep(156);
             JobCHMap.putCHMap(System.currentTimeMillis(),"mess5");
             cleanerOnTime();
+
             Thread.sleep(300);
             JobCHMap.putCHMap(System.currentTimeMillis(),"mess6");
             cleanerOnTime();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         monitor = true;
     }
     public static void cleanerOnTime(){
-        System.out.println(JobCHMap.getCHMap());
+        clHour.interrupt();
+       // System.out.println(JobCHMap.getCHMap());
       long timeNow = System.currentTimeMillis();
         Set<Long> keyset=JobCHMap.getCHMap().keySet();
         for ( Long key : keyset){
-            System.out.println(key);
+           // System.out.println(key);
             if (key < timeNow-1000){
                JobCHMap.removeCHMap(key);
             }
         }
         System.out.println(JobCHMap.getCHMap());
         monitor = false;
+        if (clHour.isInterrupted()){
+        clHour = new CleanerOneHour();
+        clHour.start();
+        System.out.println("zapusk ");}
         }
       public static void main(String[] args) {
         messagesCome();
